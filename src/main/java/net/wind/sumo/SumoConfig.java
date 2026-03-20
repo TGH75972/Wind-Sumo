c6803025aed889208ba74c6796ed2f58b1262600
TGH75972 <darknode369@gmail.com>
2026-03-20 21:30:11 +0530

    Commit 3

--- src/main/java/net/wind/sumo/SumoConfig.java
+++ src/main/java/net/wind/sumo/SumoConfig.java
@@ -1,7 +1,7 @@
 package net.wind.sumo;
 import net.minecraft.util.math.BlockPos;
 public class SumoConfig{
-public static final BlockPos LOBBY_SPAWN = new BlockPos(0, 100, 0); 
+public static final BlockPos LOBBY_SPAWN = new BlockPos(1, 67, -19); 
 public static final BlockPos SPAWN_P1 = new BlockPos(7, 71, 0);
 public static final BlockPos SPAWN_P2 = new BlockPos(-6, 71, 0);
 public static final BlockPos SPAWN_P3 = new BlockPos(0, 71, -6);

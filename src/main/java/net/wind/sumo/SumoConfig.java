e548be9c10a186e48bd9b5a81b81c9dd5a5032fd
TGH75972 <darknode369@gmail.com>
2026-03-20 19:23:36 +0530

    Commit 1

---
+++ src/main/java/net/wind/sumo/SumoConfig.java
@@ -0,0 +1,9 @@
+package net.wind.sumo;
+import net.minecraft.util.math.BlockPos;
+public class SumoConfig{
+    public static final BlockPos START_BUTTON = new BlockPos(1, 67, -19);
+    public static final BlockPos LOBBY_SPAWN = new BlockPos(0, 100, 0); 
+    public static final BlockPos SPAWN_P1 = new BlockPos(7, 71, 0);
+    public static final BlockPos SPAWN_P2 = new BlockPos(-6, 71, 0);
+    public static final BlockPos SPAWN_P3 = new BlockPos(0, 71, -6);
+}
---
+++ src/main/java/net/wind/sumo/SumoGameManager.java
@@ -0,0 +1,5 @@
+package net.wind.sumo;
+
+public class SumoGameManager {
+    
+}
